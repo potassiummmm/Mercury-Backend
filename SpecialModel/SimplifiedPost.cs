d8b829d7ad97aab7c6962cb46e93e2c09198a7e5
ZhichenRen <1337795211@qq.com>
2021-07-14 00:33:29 +0800

    Mode files

---
+++ SpecialModel/SimplifiedPost.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mercury_Backend.Models
+{
+    public class SimplifiedPost
+    {
+        public string PostId { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string SenderId { get; set; }
+        public string AvatarPath { get; set; }
+
+        public SimplifiedPost(string postId, string title, string name, string content, string senderId, string avatarPath)
+        {
+            PostId = postId;
+            Title = title;
+            Name = name;
+            SenderId = senderId;
+            AvatarPath = avatarPath;
+            if (content.Length > 30)
+            {
+                Content = content.Substring(0, 30) + "...";
+            }
+            else
+            {
+                Content = content;
+            }
+        }
+    }
+}
---
+++ SpecialModel/SimplifiedUser.cs
@@ -0,0 +1,20 @@
+﻿namespace Mercury_Backend.Models
+{
+    public class SimplifiedUser
+    {
+        public string Id { get; set; }
+        public string NickName { get; set; }
+        public string AvatarPath { get; set; }
+        public string RealName { get; set; }
+        public string Role { get; set; }
+
+        public SimplifiedUser(string id, string nickName, string avatarPath, string realName, string role)
+        {
+            Id = id;
+            NickName = nickName;
+            AvatarPath = avatarPath;
+            RealName = realName;
+            Role = role;
+        }
+    }
+}
