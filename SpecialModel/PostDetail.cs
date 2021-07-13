c61e4a858ac5d5f75a90d51a8f1f90a8303c2b9a
ZhichenRen <1337795211@qq.com>
2021-07-14 00:33:12 +0800

    Create classes

---
+++ SpecialModel/PostDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mercury_Backend.Models
+{
+    public class PostDetail
+    {
+        public string Id { get; set; }
+        public string SenderId { get; set; }
+        public string SenderName { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Time { get; set; }
+        public string AvatarPath { get; set; }
+        public List<SimplifiedComment> Comments { get; set; }
+        public List<string> Images { get; set; }
+
+        public PostDetail(string id, string senderId, string senderName, string title, string content, DateTime time, string avatarPath, List<SimplifiedComment> comments, List<string> images)
+        {
+            Id = id;
+            SenderId = senderId;
+            SenderName = senderName;
+            Title = title;
+            Content = content;
+            Time = time;
+            AvatarPath = avatarPath;
+            Comments = comments;
+            Images = images;
+        }
+    }
+}
---
+++ SpecialModel/SimplifiedComment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Mercury_Backend.Models
+{
+    public class SimplifiedComment
+    {
+        public string CommentId { get; set; }
+        public string SenderId { get; set; }
+        public string SenderName { get; set; }
+        public string AvatarPath { get; set; }
+        public DateTime Time { get; set; }
+        public string Content { get; set; }
+
+        public SimplifiedComment(string commentId, string senderId, string senderName, string avatarPath, DateTime time, string content)
+        {
+            CommentId = commentId;
+            SenderId = senderId;
+            SenderName = senderName;
+            AvatarPath = avatarPath;
+            Time = time;
+            Content = content;
+        }
+    }
+}
