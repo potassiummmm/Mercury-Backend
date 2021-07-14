1aec7539b99bd7bf841aa2c93e165d062e448c6e
ZhichenRen <1337795211@qq.com>
2021-07-14 11:00:05 +0800

    Update

--- SpecialModel/SimplifiedCommodity.cs
+++ SpecialModel/SimplifiedCommodity.cs
@@ -13,9 +13,12 @@ namespace Mercury_Backend.Models
         public int Likes { get; set; }
         public string Cover { get; set; }
         public string SellerId { get; set; }
+        public string SellerName { get; set; }
         public string SellerAvatar { get; set; }
+        public List<string> CommodityTag { get; set; }
 
-        public SimplifiedCommodity(string id, string name, decimal price, int likes, string cover, string sellerId, string sellerAvatar)
+        public SimplifiedCommodity(string id, string name, decimal price, int likes, string cover, string sellerId,
+           string sellerName,  string sellerAvatar, List<string> commodityTag)
         {
             Id = id;
             Name = name;
@@ -23,7 +26,9 @@ namespace Mercury_Backend.Models
             Likes = likes;
             Cover = cover;
             SellerId = sellerId;
+            SellerName = sellerName;
             SellerAvatar = sellerAvatar;
+            CommodityTag = commodityTag;
         }
     }
 }
